278dc5161cd5019d970154b47998c7b62f77f812
= <sewoong0409@naver.com>
2022-01-02 23:51:15 +0900

    2022-01-02 adding welcome page

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+}
