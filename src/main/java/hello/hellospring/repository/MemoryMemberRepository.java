48288469f496aded8a4eee2301c48df2819cddad
= <sewoong0409@naver.com>
2022-01-06 09:40:21 +0900

    20220106_2

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,13 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
-    @Override
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
